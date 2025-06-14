cccf13241f3d3aeb53ee2c96ea748559897e2b2f
Alexander Zlobin <th3rshifter@gmail.com>
2025-06-15 07:27:09 +1000

    NPM & Node.JS installed

--- Jenkinsfile
+++ Jenkinsfile
@@ -23,7 +23,7 @@ pipeline {
                 }
             }
         }
-
+        
         stage('Deploy to OpenShift') {
             agent any
             environment {
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', port)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(`Hello from node.js app in OpenShift!!!\nAPI_KEY: ${process.env.API_KEY}`)
+  response.send(`Hello from node.js app in OpenShift\nAPI_KEY: ${process.env.API_KEY}`)
 })
 
 app.listen(app.get('port'), function() {
